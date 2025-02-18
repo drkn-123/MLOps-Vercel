e863015ce8b958e74639f44e8b2412ce49291ab2
drkn-123 <136154911+drkn-123@users.noreply.github.com>
2025-02-18 13:31:39 +0500

    Create flask_server.py

---
+++ backend/flask_server.py
@@ -0,0 +1,19 @@
+import os
+import flask
+import flask_cors
+import sys
+
+# create a new flask app
+app = flask.Flask(__name__)
+flask_cors.CORS(app)
+
+
+# route to print hellow world
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+    
